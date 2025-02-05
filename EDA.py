6b22c7d096cb4d24903e84d2fe70ce1a40aaf0b1
sahirr07 <146308457+sahirr07@users.noreply.github.com>
2025-02-05 21:34:12 +0530

    Add files via upload

---
+++ EDA.py
@@ -0,0 +1,12 @@
+import sweetviz as sv
+import pandas as pd
+
+file_path = './MLZilla Data Set.xlsx' 
+data = pd.read_excel(file_path)
+if 'TotalCharges' in data.columns:
+    data['TotalCharges'] = pd.to_numeric(data['TotalCharges'], errors='coerce')
+report = sv.analyze(data)
+
+report.show_html('MLZilla_EDA_Report.html')
+
+print("EDA report generated successfully: MLZilla_EDA_Report.html")
